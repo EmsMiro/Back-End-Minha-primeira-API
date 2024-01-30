76194feba7ac8d030ff46435bd1ae3ca01d97c18
EmsMiro <ednirlicosta@gmail.com>
2024-01-29 21:28:47 -0300

    usando método post para a função criaMulher

--- mulheres.js
+++ mulheres.js
@@ -1,45 +1,75 @@
-const express = require("express")
+const express = require("express") //iniciando o express
 
-const router = express.Router()
+const router = express.Router() //configurando a primeira parte da rota
 
-const app = express()
+const { v4: uuidv4 } = require('uuid'); //iniciando a biblioteca uuid
 
-const porta = 3333
 
-const mulheres = [
+const app = express()//iniciando o app
+app.use(express.json()) // tratando os dados que vão trafegar a partir da requisição em formato Jsoon
+
+const porta = 3333 //criando a porta
+
+app.use((req, res, next) => {
+    console.log(req.body);
+    next();
+  });
+
+// criando lista incial de mulheres
+const listaMulheres = [
 
     {
-
+        id: '1',
         nome: 'Simara Conceição',
         imagem: 'https://bit.ly/3LJIyOF',
-        minibio: 'Desenvolvedora e instrutora',
+        minibio: 'Desenvolvedora e instrutora'
 
     },
 
     {
-
+        id: '2',
         nome: 'Iana Chan',
         imagem: 'https://bit.ly/3JCXBqP',
-        minibio: 'CEO & Founder da PrograMaria',
+        minibio: 'CEO & Founder da PrograMaria'
 
     },
 
     {
-
+        id: '3',
         nome: 'Luana Pimentel',
         imagem: 'https://bit.ly/3FKpFaz',
-        minibio: 'Senior Staff Software Engineer',
+        minibio: 'Senior Staff Software Engineer'
 
     }
 ]
 
+// GET
 function mostraMulheres(request, response) {
-    response.json(mulheres)
+    response.json(listaMulheres)
 }
 
+// POST
+function criaMulher(request, response) {
+    const novaMulher = {
+
+        id: uuidv4(),     
+        nome: request.body.nome,     
+        imagem: request.body.imagem,     
+        minibio: request.body.minibio 
+
+      }
+
+    listaMulheres.push(novaMulher);
+
+    response.json(listaMulheres)
+}
+
+// função mostra a porta
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+
+app.use(router.get('/mulheres', mostraMulheres)) // segunda parte da configuração da rota - usando GET 
+app.use(router.post('/mulheres', criaMulher)) // 2ª parte da config. da rota - usando POST
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
